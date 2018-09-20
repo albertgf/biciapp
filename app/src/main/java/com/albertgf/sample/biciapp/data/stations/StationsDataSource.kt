dfe0a313a05130d7d9ff4700afa071c0b662eddf
albert-win <albertgfdev@gmail.com>
2018-09-20 12:03:09 +0200

    ADD stations datasource

--- app/src/main/java/com/albertgf/sample/biciapp/data/stations/StationsDataSource.kt
+++ app/src/main/java/com/albertgf/sample/biciapp/data/stations/StationsDataSource.kt
@@ -1,2 +1,12 @@
 package com.albertgf.sample.biciapp.data.stations
 
+import org.funktionale.either.Either
+
+interface StationsDataSource {
+    fun get(key: String): Either<DomainError, StationDomain>
+    fun getAll(): Either <DomainError, List<StationMinimalDomain>>
+    fun isUpdated(): Boolean = true
+    fun populate(stations: List<StationDomain>)
+    fun contains(key: String): Boolean = false
+
+}
